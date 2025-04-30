ed27a8f6b70de4abf2f9fe4fdc1fee4bde8a2ed4
chulakasam <sampathchulaka33872@gmail.com>
2025-04-30 14:47:07 +0530

    add data source for authentication

--- src/main/java/lk/ijse/demoMVCSecurity/config/SecurityConfiguration.java
+++ src/main/java/lk/ijse/demoMVCSecurity/config/SecurityConfiguration.java
@@ -3,15 +3,17 @@ package lk.ijse.demoMVCSecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
-    @Bean
+  /*  @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
                 .username("john")
@@ -31,6 +33,11 @@ public class SecurityConfiguration {
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
 
+   */
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
     @Bean
     public SecurityFilterChain  filterChain(HttpSecurity http) throws Exception {
 
